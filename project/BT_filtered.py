0f236bf36e00c0a02d12afaa462503abf576196b
itapper <itapper@kth.se>
2021-12-28 22:49:00 +0100

    filtering history data

--- project/BT_filtered.py
+++ project/BT_filtered.py
@@ -1,4 +1,22 @@
 import pandas as pd
-bt_history = pd.read_csv("NetflixViewingHistory.csv")
-movies = pd.read_csv("movie_titles.csv")
-movies.head(5)
+
+def remove_seasons(row):
+    if ": Säsong " in row['Title']:
+       return(row['Title'].split(": Säsong ")[0])
+    return row["Title"]
+
+bt_history = pd.read_csv('NetflixViewingHistory.csv', sep=';', delimiter=None, header='infer', names=None, index_col=None)
+movies = pd.read_csv("movie_titles.csv", sep=';', delimiter=None, header='infer', names=None, index_col=None)
+
+
+bt_history['Title'] = bt_history.apply (lambda row: remove_seasons(row), axis=1)
+
+
+updated_history = bt_history.merge(movies.drop_duplicates(), on = ["Title"], 
+                   how='left', indicator=True)
+
+updated_history = updated_history[updated_history["_merge"]=="both"]
+updated_history = updated_history[["Title","Date"]]
+
+print(bt_history.head(5))
+print(updated_history.head(5))
